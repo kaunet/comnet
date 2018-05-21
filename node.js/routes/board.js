81b6c7ade2a283fb7e02a87294ef627e4a325f28
rapsealk <piono623@naver.com>
2018-05-21 15:07:00 +0900

    import -> require

--- node.js/routes/board.js
+++ node.js/routes/board.js
@@ -1,6 +1,4 @@
-import express from 'express';
-//const { express } = require('express');
-// const express = require('express');
+const express = require('express');
 const router = express.Router();
 
 const database = require('../utils/database');
