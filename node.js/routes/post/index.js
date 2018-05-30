0aec28db7376c6e29b1dfe7b7f579bbcc6756190
rapsealk <piono623@naver.com>
2018-05-30 23:45:26 +0900

    add and delete comment f

--- node.js/routes/post/index.js
+++ node.js/routes/post/index.js
@@ -9,110 +9,126 @@ router.use('/new', require('./new'));
 
 router.get('/:id', async function(req, res) {
 
-    let user = req.session.user;
+	let user = req.session.user;
 
-    let id = req.params.id;
-    if (!id) return res.redirect('/');
+	let id = req.params.id;
+	if (!id) return res.redirect('/');
 
-    try {
-        var conn = await database.getConnection();
-        await conn.beginTransaction();
-        try {
-            let query1 = 'SELECT * FROM post WHERE PostID = ?';
-            let post = (await conn.query(query1, id)).shift();
-            
-            if (post) {
-                let query2 = 'SELECT * FROM comment WHERE PostID = ?';
-                post.comments = await conn.query(query2, post.PostID);
-                let parallel = post.comments.map(comment => {
-                    comment.TimeStamp = moment(comment.TimeStamp).format('YYYY-MM-DD HH:mm:ss');
-                });
-                await Promise.all(parallel);
-                let query3 = 'SELECT * FROM file WHERE PostID = ?';
-                post.file = (await conn.query(query3, id)).shift();
-                console.log('post.file:', post.file);
-                await conn.commit();
-                res.render('post', { user: user, post: post, timestamp: moment().format('YYYY-MM-DD HH:mm:ss') });
-            } else {
-                await conn.destroy();
-                res.redirect('/');
-            }
-        }
-        catch (error) {
-            throw error;
-        }
-    }
-    catch (error) {
-        console.log('error:', error);
-        res.redirect('/');
-    }
-    finally {
-        await database.releaseConnection(conn);
-    }
+	try {
+		var conn = await database.getConnection();
+		await conn.beginTransaction();
+		try {
+			let query1 = 'SELECT * FROM post WHERE PostID = ?';
+			let post = (await conn.query(query1, id)).shift();
+			
+			if (post) {
+				let query2 = 'SELECT * FROM comment WHERE PostID = ?';
+				post.comments = await conn.query(query2, post.PostID);
+				let parallel = post.comments.map(comment => {
+					comment.TimeStamp = moment(comment.TimeStamp).format('YYYY-MM-DD HH:mm:ss');
+				});
+				await Promise.all(parallel);
+				let query3 = 'SELECT * FROM file WHERE PostID = ?';
+				post.file = (await conn.query(query3, id)).shift();
+				console.log('post.file:', post.file);
+				await conn.commit();
+				res.render('post', { user: user, post: post, timestamp: moment().format('YYYY-MM-DD HH:mm:ss') });
+			} else {
+				await conn.destroy();
+				res.redirect('/');
+			}
+		}
+		catch (error) {
+			throw error;
+		}
+	}
+	catch (error) {
+		console.log('error:', error);
+		res.redirect('/');
+	}
+	finally {
+		await database.releaseConnection(conn);
+	}
 });
 
 /*
 router.post('/file', multer.single('file'), function(req, res) {
-    console.log('req.file:', req.file);
-    res.json({ succeed: true, url: req.file.location });
+	console.log('req.file:', req.file);
+	res.json({ succeed: true, url: req.file.location });
 });
 */
 
 router.delete('/:id/comment', async function(req, res) {
 
-    let user = req.session.user;
+	let user = req.session.user;
 
-    let id = req.params.id;
-    let commentId = req.body.commentId;
-    console.log('commentId:', commentId);
-    
-    try {
-        var conn = await database.getConnection();
-        let query = 'DELETE FROM comment WHERE CommentId = ?';
-        let result = await conn.query(query, commentId);
-        console.log('result:', result);
-    }
-    catch (error) {
-        console.error(error);
-    }
-    finally {
-        await database.releaseConnection(conn);
-    }
+	let id = req.params.id;
+	let commentId = req.body.commentId;
+	console.log('commentId:', commentId);
+	
+	try {
+		var conn = await database.getConnection();
+		let query = 'DELETE FROM comment WHERE CommentId = ?';
+		let result = await conn.query(query, commentId);
+		console.log('result:', result);
+	}
+	catch (error) {
+		console.error(error);
+	}
+	finally {
+		await database.releaseConnection(conn);
+	}
 
-    res.json({ succeed: true, commentId: commentId });
+	res.json({ succeed: true, commentId: commentId });
 });
 
 router.post('/:id/comment', async function(req, res){
-    let user = req.session.user;
-    let id = req.params.id;
-    
-    let body = req.body.comment;
+	
+	let user = req.session.user;
+	let id = req.params.id;
+	
+	let body = req.body.comment;
 
-    console.log('body:',body);
+	console.log('body:',body);
 
-    try{
-        var conn =await database.getConnection();
-        let query1 = 'SELECT * FROM post WHERE PostID = ?';
-        let post = (await conn.query(query1, id)).shift();
+	try {
+		var conn = await database.getConnection();
+		await conn.beginTransaction();
+		try {
+			let query1 = 'SELECT * FROM post WHERE PostID = ?';
+			let post = (await conn.query(query1, id)).shift();
 
-        let query = 'INSERT INTO comment SET ?';
-        let comment ={
-            TimeStamp: new Date(),
-            PostID: post.PostID, 
-            ID: user.ID,
-            body: body
-        };
+			let timestamp = new Date();
 
-        let result = await conn.query(query, comment);
-        console.log('result:',result);
-    }catch(error){
-        console.error(error);
-    }
-    finally{
-        await database.releaseConnection(conn);
-    }
+			let query = 'INSERT INTO comment SET ?';
+			let comment = {
+				TimeStamp: timestamp,
+				PostID: post.PostID, 
+				ID: user.ID,
+				body: body
+			};
 
-    res.json({succeed: true, comment: body});
+			let result = await conn.query(query, comment);
+			console.log('result:',result);
 
-})
+			let query2 = 'SELECT * FROM comment ORDER BY CommentID DESC';
+			let _comment = (await conn.query(query2, timestamp)).shift();
+			_comment.TimeStamp = moment(_comment.TimeStamp).format('YYYY-MM-DD HH:mm:ss');
+
+			await conn.commit();
+			res.json({ succeed: true, comment: _comment });
+		}
+		catch (error) {
+			throw error;
+		}  
+	} catch (error){
+		await conn.rollback();
+		console.error(error);
+		res.json({ succeed: false });
+	}
+	finally{
+		await database.releaseConnection(conn);
+	}
+});
+
 module.exports = router;
